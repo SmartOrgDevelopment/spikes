3d5bb6ce4e3f5fb92f9a6c3192d4b9c86418ed2f
Somik Raha <sraha@smartorg.com>
2013-02-08 17:13:02 -0800

    Working demo of MonteCarlo

---
+++ AsyncWorkQueueTest/AsyncWorkQueueTest/PointEstimate.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsyncWorkQueueTest
+{
+    public class PointEstimate
+    {
+        private double value;
+        private double k;
+        private double sum;
+        private double v;
+
+        public PointEstimate()
+        {
+            k = 0;
+            sum = 0;
+            v = 0;
+        }
+
+        public void nextValueIs(double value)
+        {
+            this.value = value;
+            calculate();
+        }
+
+        public void calculate()
+        {
+            k++;
+            if (k > 1)
+            {
+                double diff = (sum - (k - 1) * value);
+                v = ((k - 2) * v + (diff * diff) / (k * (k - 1))) / (k - 1);
+            }
+            sum += value;
+        }
+
+        public double variance()
+        {
+            return v;
+        }
+
+        public double halfIntervalFor(double z)
+        {
+            return z * Math.Sqrt(v / k);
+        }
+
+        public SummaryStatistics computeConfidenceIntervalForPercent(int percent) {
+	        double hi = halfIntervalFor(zFor(percent));
+	        double pointEstimate = mean();
+		    double cLower = pointEstimate - hi;
+		    double cUpper = pointEstimate + hi;
+            SummaryStatistics summary = new SummaryStatistics();
+		    summary.cLower = cLower;
+            summary.cUpper = cUpper;
+            summary.pointEstimate = pointEstimate;
+            return summary;
+	    }
+
+        public double zFor(int percent)
+        {
+            if (percent == 98)
+            {
+                return 2.33;
+            }
+            else if (percent == 99)
+            {
+                return 2.576;
+            }
+            else return -1;
+        }
+
+        public double mean()
+        {
+            return sum / k;
+        }
+
+        public long numberOfTrialsNeeded(double z, double epsilon)
+        {
+            double muHat = mean();
+            return (long)((v * z * z) / (epsilon * muHat * epsilon * muHat));
+        }
+
+        public double sumValue()
+        {
+            return sum;
+        }
+    }
+    public class SummaryStatistics
+    {
+        public double cLower {get; set;}
+        public double cUpper {get; set;}
+        public double pointEstimate { get; set; }
+    }
+}
--- AsyncWorkQueueTest/AsyncWorkQueueTest/Program.cs
+++ AsyncWorkQueueTest/AsyncWorkQueueTest/Program.cs
@@ -2,54 +2,79 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
+using SpreadsheetGear;
 
 namespace AsyncWorkQueueTest
 {
     class Program
     {
+        private static double ONE_MILLION = 1000000.0;
+        public static int generate(int low, int high)
+        {
+            Random random = new Random();
+            return random.Next(low, high);
+        }
         public static void Main(string[] args)
         {
-            using (var q = new AsyncWorkQueue<double>(5))
+            PointEstimate estimate = new PointEstimate();
+            Stopwatch sw = Stopwatch.StartNew();
+            using (var q = new AsyncWorkQueue<double>(3))
              {
-                 for (int i = 0; i < 12; i++)
-                 {
-                     var id = i;
-                     var time = i * 100;
-                     var square = double.MaxValue - i;
-
-                     Console.WriteLine("Starting {0}", id);
-
-                     q.Start(delegate
-                     {
-                         Console.WriteLine("COMPUTING ...  {0}", id);
-                         Thread.Sleep(time);
-                         //throw new Exception("blah");
-                         var result = Math.Sqrt(square);
-                         Console.WriteLine("Done {0}", id);
-                         return result;
-                     });
-                 }
-
+                for (int i=0;i<3;i++) {
+                    // 3 processors
+                    var id = i;
+                    q.Start(delegate
+                    {
+                        SpreadsheetGear.IWorkbookSet workbookSet = SpreadsheetGear.Factory.GetWorkbookSet();
+                        IWorkbook workbook = workbookSet.Workbooks.Open(@"C:\data\dev\Async\AsyncWorkQueueTest\AsyncWorkQueueTest\excel\helloWorld_template.xls");
+                        for (long j = 0; j < ONE_MILLION/3; j++)
+                        {
+                            try
+                            {
+                                double annRevenue = generate(30, 70);
+                                double annCosts = generate(5, 20);
+                                double numYears = generate(2, 6);
+                                var sheet = workbook.Worksheets["Hello"];
+                                sheet.Cells["AnnRevenue"].Value = Convert.ToDouble(annRevenue);
+                                sheet.Cells["AnnCosts"].Value = Convert.ToDouble(annCosts);
+                                sheet.Cells["numYears"].Value = Convert.ToDouble(numYears);
+                                var npvRange = sheet.Cells["npv"];
+                                double npv = (double)npvRange.Value;
+                                estimate.nextValueIs(npv);
+                                /*if (j % 50000 == 0)
+                                {
+                                    Console.WriteLine("Process "+id+ ", Runs: "+ j);
+                                }*/
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Process " + id + ", run " + j + ", exception thrown!");
+                                Console.WriteLine(e.StackTrace);
+                            }
+                        }
+                        return 0;
+                    });
+                    Console.WriteLine("Started delegate " + i);
+                }
+                
                  Console.WriteLine("*** Done queueing.");
+                
 
-                 int count = 0;
                  foreach (var result in q.GetResults())
                  {
                      if (result.IsError)
                          //handle / log errors here
                          throw result.Error;
-
-                     Console.WriteLine(result.Value);
-
-                     count++;
                  }
-
-                 Console.WriteLine("\n###\nCount = {0}", count);
+                 SummaryStatistics statistics = estimate.computeConfidenceIntervalForPercent(95);
+                 sw.Stop();
+                 Console.WriteLine("Mean: " + statistics.pointEstimate + ", [" + statistics.cLower + ", " + statistics.cUpper + "]");
+                 Console.WriteLine("Elapsed time = " + sw.Elapsed);
              }
 
             
             
-            Test(new List<int>() { 5, 6, 5, 4, 3, 2, 1 });
+            //Test(new List<int>() { 5, 6, 5, 4, 3, 2, 1 });
              
             Console.ReadLine();
 
