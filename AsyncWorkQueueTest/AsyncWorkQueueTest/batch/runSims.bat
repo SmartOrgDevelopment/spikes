fb12d69aa5ff9b766da09e10d65e8a39a39c1848
Somik Raha <sraha@smartorg.com>
2013-02-13 11:30:31 -0800

    Added batch file

---
+++ AsyncWorkQueueTest/AsyncWorkQueueTest/batch/runSims.bat
@@ -0,0 +1,6 @@
+cd c:\data\dev\Async\AsyncWorkQueueTest\AsyncWorkQueueTest\excel
+..\bin\Debug\AsyncWorkQueueTest.exe 1 1000000
+..\bin\Debug\AsyncWorkQueueTest.exe 2 1000000
+..\bin\Debug\AsyncWorkQueueTest.exe 3 1000000
+..\bin\Debug\AsyncWorkQueueTest.exe 4 1000000
+cd c:\data\dev\Async\AsyncWorkQueueTest\AsyncWorkQueueTest\batch
